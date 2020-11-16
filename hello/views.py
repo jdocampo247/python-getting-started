a2fae7fa4c11ef9cb0dbcafff0dadaef04bcb9d5
Julian David Ocampo Herrera <jd.ocampo247@uniandes.edu.co>
2020-11-15 19:01:43 -0500

    Add caching with MemCachier

--- hello/views.py
+++ hello/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import requests
+import os
+from django.core.cache import cache
+import time
 
 from .models import Greeting
 
-# Create your views here.
+TASKS_KEY = "tasks.all"
+
 def index(request):
+    tasks = cache.get(TASKS_KEY)
+    if not tasks:
+        time.sleep(2)  # simulate a slow query.
+        tasks = 'Tarea'
+        cache.set(TASKS_KEY, tasks)
+        print("Usado cache")
     r = requests.get('http://httpbin.org/status/418')
     print(r.text)
     return HttpResponse('<pre>' + r.text + '</pre>')
+
+# Create your views here.
+#def index(request):
+    #times = int(os.environ.get('TIMES', 3))
+    #return HttpResponse('Hello! ' * times)
+
+    #r = requests.get('http://httpbin.org/status/418')
+    #print(r.text)
+    #return HttpResponse('<pre>' + r.text + '</pre>')
 
     # return HttpResponse('Hello from Python!')
     #return render(request, "index.html")
